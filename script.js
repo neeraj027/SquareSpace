325bd4f7fbbd4a6bbfa9516d7e985536a73a1328
neeraj027 <neerajkshatriy@gmail.com>
2023-08-25 00:47:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var tl = gsap.timeline()
+tl.from(".nav img,.nav h3 ,.nav button",{
+    y:-100,
+    opacity:0,
+    duration: .3,
+    delay:.2,
+    stagger:.1
+})
+
+tl.from("h1",{
+    y:100,
+    duration:.4,
+    stagger: .1,
+    opacity:0
+})
+
+tl.from(".img",{
+    x:-100,
+    opacity:0,
+    duration:.5,
+    stagger: .3  
+})
